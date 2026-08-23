1f840eeaf3002964a647f290a3ff427657145bf1
BADR RAMI <badrrami2004@gmail.com>
2026-08-23 15:06:46 +0100

    ajouter le fichier qui est chargé de traiter les requestes http et laicer le fichier de serveur gere juste le serveur

---
+++ server/EtudiantHandler.java
@@ -0,0 +1,14 @@
+package server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class EtudiantHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
--- server/ServeurHTTP.java
+++ server/ServeurHTTP.java
@@ -1,5 +1,22 @@
 package server;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class ServeurHTTP {
-    
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8081),
+                0
+        );
+
+        System.out.println("Serveur HTTP démarré sur le port 8081");
+
+        server.createContext("/etudiants", new EtudiantHandler());
+
+        server.start();
+    }
 }
